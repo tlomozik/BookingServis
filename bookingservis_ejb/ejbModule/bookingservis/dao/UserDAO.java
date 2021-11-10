6b51a74ca2f7c307ff180b2c9e12b328eaf941a3
tlomozik <tymek994@gmail.com>
2021-11-10 23:11:19 +0100

    Second commit

--- bookingservis_ejb/ejbModule/bookingservis/dao/UserDAO.java
+++ bookingservis_ejb/ejbModule/bookingservis/dao/UserDAO.java
@@ -1,8 +1,14 @@
 package bookingservis.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import bookingservis.dao.UserDAO;
 
 import bookingservis.entities.User;
 
@@ -30,4 +36,63 @@ public class UserDAO {
 		return em.find(User.class, id);
 	}
 	
+	
+	
+	public List<User> getFullList() {
+		List<User> list = null;
+
+		Query query = em.createQuery("select u from User u");
+
+		try {
+			list = query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+	public List<User> getList(Map<String, Object> searchParams) {
+		List<User> list = null;
+
+		// 1. Build query string with parameters
+		String select = "select u ";
+		String from = "from User u ";
+		String where = "";
+		String orderby = "order by u.surname asc, u.name";
+
+		// search for surname
+		String surname = (String) searchParams.get("surname");
+		if (surname != null) {
+			if (where.isEmpty()) {
+				where = "where ";
+			} else {
+				where += "and ";
+			}
+			where += "u.surname like :surname ";
+		}
+		
+		// ... other parameters ... 
+
+		// 2. Create query object
+		Query query = em.createQuery(select + from + where + orderby);
+
+		// 3. Set configured parameters
+		if (surname != null) {
+			query.setParameter("surname", surname+"%");
+		}
+
+		// ... other parameters ... 
+
+		// 4. Execute query and retrieve list of Person objects
+		try {
+			list = query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
 }
+
